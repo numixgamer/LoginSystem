5724b156223122501d84f93d389137e9d894ebff
numixgamer <hadi.hadi.hadi.567@gmail.com>
2026-01-08 15:24:10 +0330

    برنامه ساخته شد

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # code
+
+username = 'Javad'
+password = 'javad123'
+
+
+u = input("username :")
+p = input("password :")
+
+if u == username:
+    if p == password:
+        print("Wellcome !")
+    else:
+        print("Password eshtebah")
+else:
+    if p == password:
+        print("username eshtebah")
+    else:
+        print("password ham eshtebahe dalgak")
